69f290a480f86a64e3023236b93cca1fed169d6a
Chercheuse <108093435+Chercheuse@users.noreply.github.com>
2023-11-19 19:29:51 +0400

    task 3 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,21 @@
-function getElementWidth(content, padding, border) {
-  return (
-    Number.parseInt(content) +
-    Number.parseInt(padding) * 2 +
-    Number.parseFloat(border) * 2
-  );
-}
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
 
-console.log(Number.parseInt(getElementWidth("50px", "8px", "4px"))); // 74
-console.log(Number.parseInt(getElementWidth("60px", "12px", "8.5px"))); // 101
-console.log(Number.parseInt(getElementWidth("200px", "0px", "0px"))); // 200
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
